40adf19b0baa59da5fb0b8b8b63d0e52dc8697c6
Tim 'mithro' Ansell <me@mith.ro>
2020-11-10 05:40:50 -0800

    Release of a subset of IO into sky130_fd_io 0.1.0
    This release includes;
     * Power pads;
        - High voltage - `top_power_hvc_wpad`, `top_ground_hvc_wpad`
        - Low voltage - `top_power_lvc_wpad`, `top_ground_lvc_wpad`
     * Reference generators - `top_refgen`, `top_refgen_new`.
     * Standard GPIO (`top_gpiov2`).
     * I2C compatible GPIO with over-voltage tolerance (`top_gpio_ovtv2`).
     * Special SIO (`top_sio`) with differential macro (`top_sio_macro`).
     * System wide reset influencing XRES cell (`top_xres4v2`).
    
    Future release will include;
     * Original standard GPIO (`top_gpio`).
     * More power pad versions.
     * More XRES cells.
     * More reference generators (`top_vrefcapv2`, etc).
     * Spice and CDL models for the IOs.
    
    This commit contains major improvements to all files in all modules by
    regenerating from original data, improving consistency and automated cross
    checking of data.
    
    These improvements should drastically reduce customer confusion when
    using the library and further reduce future possibility for human errors to
    creep into designs.
    
    Notable improvements include;
    
     * A large number of files have been regenerated from original source
       data including most liberty timing files and spice simulation models
       (compared to previous hand created versions).
    
     * Catalog and other library aggregations are now automatically
       generated from library contents (compared to previous hand created
       versions).
    
     * Significant improvements to documentation for all cells, including
       producing graphical representations, verified metadata and
       descriptions.
    
     * Names have been cross referenced between file types (such as
       simulation, layout, schematic and timing) and now verified to match.
    
     * Names have been improved to fix a standard format across all supported
       libraries and PDK contents.
    
     * Significant improvements to the contents of text files (like the
       verilog files) through improving consistent style that has been
       automatically checked.
    
     * Simplified verilog files for usage with open tools, including new
       black box stubs have been created.
    
     * Too many numerous other changes to list here.
    
    Signed-off-by: Tim 'mithro' Ansell <tansell@google.com>

---
+++ cells/top_refgen/sky130_fd_io__top_refgen.functional.v
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2020 The SkyWater PDK Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+*/
+
+module sky130_fd_io__top_refgen (VINREF, VOUTREF, REFLEAK_BIAS,
+                                 HLD_H_N, IBUF_SEL, OD_H, VOHREF, VREF_SEL, VREG_EN, VTRIP_SEL);
+wire error_vsel;
+output VINREF;
+output VOUTREF;
+inout REFLEAK_BIAS;
+wire vccd	=1;
+wire vcchib	=1;
+wire vdda	=1;
+wire vddio	=1;
+wire vddio_q=1;
+wire vssd	=0;
+wire vssio	=0;
+wire vssio_q=0;
+input HLD_H_N;
+input IBUF_SEL;
+input OD_H;
+input VOHREF;
+input VREF_SEL;
+input VREG_EN;
+input VTRIP_SEL;
+reg [2:0] dm_final;
+reg       slow_final, vtrip_sel_final, inp_dis_final, hld_ovr_final;
+reg [2:0] dm;
+reg       slow, inp_dis, hld_ovr;
+reg [1:0] vsel;
+wire  pwr_good_hold_mode	= 1;
+wire  pwr_good_active_mode      = 1;
+always @(*)
+begin
+    if (^OD_H===1'bx || !pwr_good_hold_mode || (OD_H===0 && ^HLD_H_N===1'bx))
+    begin
+        dm_final 	= 3'bxxx;
+        slow_final 	= 1'bx;
+        vtrip_sel_final	= 1'bx;
+        inp_dis_final 	= 1'bx;
+        hld_ovr_final 	= 1'bx;
+    end
+    else if (OD_H===1)
+    begin
+        dm_final 	= 3'b000;
+        slow_final 	= 1'b0;
+        vtrip_sel_final	= 1'b0;
+        inp_dis_final 	= 1'b0;
+        hld_ovr_final 	= 1'b0;
+    end
+    else if (HLD_H_N===1)
+    begin
+        dm_final 	= (^dm[2:0] === 1'bx	|| !pwr_good_active_mode) ? 3'bxxx : dm;
+        slow_final 	= (^slow 	=== 1'bx	|| !pwr_good_active_mode) ? 1'bx   : slow;
+        vtrip_sel_final	= (^VTRIP_SEL 	=== 1'bx	|| !pwr_good_active_mode) ? 1'bx   : VTRIP_SEL;
+        inp_dis_final 	= (^inp_dis	=== 1'bx	|| !pwr_good_active_mode) ? 1'bx   : inp_dis;
+        hld_ovr_final 	= (^hld_ovr 	=== 1'bx	|| !pwr_good_active_mode) ? 1'bx   : hld_ovr;
+    end
+end
+reg dis_err_msgs;
+initial
+begin
+    dis_err_msgs = 1'b1;
+`ifdef SKY130_FD_IO_TOP_REFGEN_DIS_ERR_MSGS
+`else
+    #1;
+    dis_err_msgs = 1'b0;
+`endif
+end
+assign error_vsel = (vsel[1]===1 && vsel[0]===1);
+always @(*)
+begin
+    if (!dis_err_msgs)
+    begin
+        if (error_vsel==1) $display(" ===ERROR=== sky130_fd_io__top_refgen : %m : Incorrect inputs on vsel[1:0] = 11",$stime);
+    end
+end
+endmodule
